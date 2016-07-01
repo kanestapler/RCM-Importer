cf1162efd57ed1a4127bc3faafecc208459ab41c
kanestapler <kanestapler@gmail.com>
2016-06-30 22:44:51 -0500

    playing around with table names

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,10 +1,22 @@
 package main;
 
+import java.io.File;
+import java.io.IOException;
+import java.util.Set;
+
+import com.healthmarketscience.jackcess.Database;
+import com.healthmarketscience.jackcess.DatabaseBuilder;
+
 public class Main {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	public static void main(String[] args) throws IOException {
+		File backEnd = new File("C:/Users/Kane/Desktop/FileDepot/RCM_BE/RCM_BE.mdb");
+		File frontEnd = new File("C:/Users/Kane/Desktop/FileDepot/RCM_FE/RCM_FE.mdb");
+		Database db = DatabaseBuilder.open(backEnd);
+		Set<String> allTableNames = db.getTableNames();
+		for (String s : allTableNames) {
+			System.out.println(s);
+		}
 	}
 
 }
